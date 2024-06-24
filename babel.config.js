853ebbabfbfcdf77a89d1a69af721f1b6759e9bb
Hyj <2856042536@qq.com>
2024-06-24 13:34:08 +0800

    引入文本编辑器，代码编辑器

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
-module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = ["@vue/cli-plugin-babel/preset"];
+
+  const plugins = [
+    // 在这里添加静态类块转换插件
+    "@babel/plugin-transform-class-static-block",
+    // ... 你可能已经有的其他插件
+  ];
+
+  return {
+    presets,
+    plugins,
+  };
 };
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import App from "./App.vue";
 import ArcoVue from "@arco-design/web-vue";
 import "@arco-design/web-vue/dist/arco.css";
 import router from "./router";
-import store from "./store";
+import "@/plugins/axios";
+import "@/access";
+import store from "@/store";
+// 额外引入图标库
+import ArcoVueIcon from "@arco-design/web-vue/es/icon";
+import "bytemd/dist/index.css";
 
-createApp(App).use(ArcoVue).use(store).use(router).mount("#app");
+createApp(App)
+  .use(ArcoVue)
+  .use(store)
+  .use(router)
+  .use(ArcoVueIcon)
+  .mount("#app");
